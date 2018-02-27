96eb3a51ca78fb3a7ef7df9cfe9b8546defd7c11
Sarah Gardiner <sgardine@umw.edu>
2018-02-27 11:54:24 -0500

    Added Splash and beginnings of a login page

--- app/src/main/java/com/example/sarahgardiner/sample430/SplashPage.java
+++ app/src/main/java/com/example/sarahgardiner/sample430/SplashPage.java
@@ -1,7 +1,9 @@
 package com.example.sarahgardiner.sample430;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashPage extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SplashPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_page);
+    }
+
+    public void onSplashPageClick(View view) {
+        Intent intent = new Intent(this, LoginPage.class);
+        startActivity(intent);
+        finish();
     }
 }
