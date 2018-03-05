301a80989b6f1ae6a7e1df02b0c638c0a25e125d
Sarah Gardiner <sgardine@umw.edu>
2018-03-05 12:31:15 -0500

    Added Login page and started Admin Menu page

--- app/src/main/java/com/example/sarahgardiner/sample430/LoginPage.java
+++ app/src/main/java/com/example/sarahgardiner/sample430/LoginPage.java
@@ -1,7 +1,9 @@
 package com.example.sarahgardiner.sample430;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -9,6 +11,18 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+    }
+
+    public void onUserLoginClick(View view) {
+        Intent intent = new Intent(this, adminMenu.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onAdminLoginClick(View view) {
+        Intent intent = new Intent(this, adminMenu.class);
+        startActivity(intent);
+        finish();
     }
 }
 /*
